26b4aa7d1566c858a9bad9f8bc37605d8301c91b
magajisuleiman <magajisuleiman1203@gmail.com>
2024-02-01 05:47:41 +0100

    initial commit

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,30 +1,25 @@
-#!/usr/bin/python3
-""" BaseCaching module
+#!/usr/bin/env python3
+
+"""Task 0: Basic dictionary
 """
 
 
-class BaseCaching:
-    """BaseCaching defines:
-    - constants of your caching system
-    - where your data are stored (in a dictionary)
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """A class `BasicCache` that inherits from `BaseCaching`
+    and is a caching system
     """
 
-    MAX_ITEMS = 4
-
-    def __init__(self):
-        """Initiliaze"""
-        self.cache_data = {}
-
-    def print_cache(self):
-        """Print the cache"""
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
-
     def put(self, key, item):
-        """Add an item in the cache"""
-        raise NotImplementedError("put must be implemented in your cache class")
+        """assign to the dictionary `self.cache_data` the
+        `item` value for the key `key`
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
 
     def get(self, key):
-        """Get an item by key"""
-        raise NotImplementedError("get must be implemented in your cache class")
+        """return the value in `self.cache_data` linked to `key`"""
+
+        return self.cache_data.get(key, None)
