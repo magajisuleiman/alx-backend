9b5ad91529f2ec131f00c412781da6113fca0f5b
magaji <magajisuleiman1203@gmail.com>
2024-02-07 05:56:05 +0100

    task 2

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
-'''Task 0: Basic Flask app
-'''
+"""Task 0: Basic Flask app
+"""
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    '''default route'''
-    return render_template("0-index.html",)
+    """default route"""
+    return render_template(
+        "0-index.html",
+    )
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
